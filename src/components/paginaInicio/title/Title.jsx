d94c442c53bb21cfb7f7858d5f4464923eb2e330
CrisSaavedra <crisisaave@gmail.com>
2023-04-10 14:51:54 -0400

    first page done

---
+++ src/components/paginaInicio/title/Title.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Title = () => {
+    return (
+        <div className='container-title'>
+            <div className='title'>
+                <h2 className='title-1'>HUMANO</h2>
+                <h1 className='title-2'>BARBER</h1>
+            </div>
+            <div className='subtitle'></div>
+            <a className='subtitle' href="#">Agendar Hora</a>
+        </div>
+    )
+}
