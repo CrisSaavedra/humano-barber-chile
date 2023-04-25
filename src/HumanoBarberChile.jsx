61ce3e6531fd9e10f5ee743c43f1ad039b2233d8
CrisSaavedra <crisisaave@gmail.com>
2023-04-25 18:54:40 -0400

    fixed the animation navbar full screen

--- src/HumanoBarberChile.jsx
+++ src/HumanoBarberChile.jsx
@@ -3,7 +3,7 @@ import { PaginaInicio } from './components/paginaInicio/PaginaInicio'
 import { Nosotros } from './components/nosotros/Nosotros'
 import { Horario } from './components/horario/Horario'
 import { useEffect } from 'react'
-import { addEventeScrollToNavbar } from './animation'
+import { addEventWhenScroll } from './animation'
 import { Footer } from './components/footer/Footer'
 import { Mapa } from './components/mapa/Mapa'
 import { Galerias } from './components/galeria2/Galerias';
@@ -14,7 +14,7 @@ export const HumanoBarberChile = () => {
 
 
     useEffect(() => {
-        addEventeScrollToNavbar();
+        addEventWhenScroll();
     }, []);
 
 
--- src/animation.js
+++ src/animation.js
@@ -1,21 +1,31 @@
-export const addEventeScrollToNavbar = () => {
+export const addEventWhenScroll = () => {
     let navbar = document.getElementById('navbar');
 
 
     window.addEventListener("scroll", (e) => {
         let scroll = window.scrollY;
-        if (scroll > 80) {
+        if (scroll > 500) {
             navbar.style.position = 'fixed';
             navbar.style.backgroundColor = 'var(--black)';
             navbar.style.opacity = '0.9';
-
+            navbar.classList.add('on-fixed');
+            navbar.classList.remove('initial-state');
         }
-        if (scroll < 80) {
-            navbar.style.position = 'relative';
-            navbar.style.backgroundColor = 'transparent';
-            navbar.style.opacity = '1';
-
+        if (scroll < 470) {
+            navbar.classList.add('initial-state');
+            navbar.classList.remove('on-fixed');
         }
 
     })
+
+    navbar.addEventListener('animationend', (e) => {
+
+        if (e.animationName === 'initialState') {
+            navbar.style.position = 'relative';
+            navbar.style.backgroundColor = 'transparent';
+            navbar.style.opacity = '1';
+        }
+    })
 }
+
+
