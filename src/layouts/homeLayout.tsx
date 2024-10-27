fc61da51e6e6f294270036e90aae16a8737c1afa
CrisSaavedra <crisisaave@gmail.com>
2024-10-27 18:18:22 -0400

    Add SectionSix to HomeLayout for improved content structure and user engagement, enhancing the overall layout

--- src/layouts/homeLayout.tsx
+++ src/layouts/homeLayout.tsx
@@ -4,13 +4,14 @@ import SectionTwo from "@/pages/sectionTwo";
 import SectionThree from "@/pages/sectionThree";
 import SectionFour from "@/pages/sectionFour";
 import SectionFive from "@/pages/sectionFive";
+import SectionSix from "@/pages/sectionSix";
 
 function HomeLayout() {
 
 
     return (
         <>
-            <main className="font-kanit">
+            <main className="font-kanit z-[-22]">
                 <div className="h-dvh bg-[url('src/assets/backgorund-image.webp')] bg-cover bg-center absolute w-full top-0 z-[-1]" style={{ filter: "brightness(.6)" }}></div>
                 <Navbar />
                 <SectionOne />
@@ -18,7 +19,8 @@ function HomeLayout() {
                 <SectionThree />
                 <SectionFour />
                 <SectionFive />
-               
+                <SectionSix />
+                
             </main>
         </>
     );
