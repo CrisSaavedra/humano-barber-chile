b665e0ba20964473e833a3f5ee03d5afa4dcf242
CrisSaavedra <crisisaave@gmail.com>
2024-11-09 23:03:49 -0300

    Add animations for burger button state change and refine sidebar toggle behavior for improved user experience and visual feedback

--- src/helpers/closeOpenSidebar.ts
+++ src/helpers/closeOpenSidebar.ts
@@ -1,11 +1,16 @@
 export const closeOpenSideBar = () => {
     const element = document.querySelector("#sidebar");
-    if(!element) return;
+    const burgerBtn = document.querySelector("#burger");
+    if (!element || !burgerBtn) return;
 
-    if(element.classList.contains('openSideBar')){
+    if (element.classList.contains('openSideBar')) {
+        burgerBtn.classList.remove('visibleToInvisible');
+        burgerBtn.classList.add('invisibleToVisile');
         element.classList.remove('openSideBar');
         element.classList.add('closeSideBar');
-    }else {
+    } else {
+        burgerBtn.classList.remove('invisibleToVisile');
+        burgerBtn.classList.add('visibleToInvisible');
         element.classList.remove('closeSideBar');
         element.classList.add('openSideBar');
     }
