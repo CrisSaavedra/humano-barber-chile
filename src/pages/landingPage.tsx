6a9a687cf30fedef6bab9c4208b5ee1f66c334c1
CrisSaavedra <crisisaave@gmail.com>
2024-11-11 20:53:43 -0400

    Update landing page link to direct users to the scheduling site for improved user navigation and experience

--- src/pages/landingPage.tsx
+++ src/pages/landingPage.tsx
@@ -7,7 +7,7 @@ function LandingPage() {
                         <h5 className="md:text-6xl text-4xl">HUMANO</h5>
                         <h4 className="md:text-9xl text-6xl font-semibold realitve">BARBER <span className="text-2xl absolute">®</span></h4>
                     </div>
-                    <a href="#" className="border-b border-yellow text-white mt-5 text-2xl ">Agendar Hora</a>
+                    <a href="https://humanosalonbarberchile.site.agendapro.com/cl/sucursal/27344" className="border-b border-yellow text-white mt-5 text-2xl ">Agendar Hora</a>
                     <div className="flex gap-5  w-full justify-end px-10 absolute bottom-10">
                         <a href="#"><svg width="30" enableBackground="new 0 0 128 128" id="Social_Icons" version="1.1" viewBox="0 0 128 128" ><g id="_x31__stroke"><g id="Facebook_1_"><rect fill="none" height="128" width="128"></rect><path clipRule="evenodd" d="M68.369,128H7.065C3.162,128,0,124.836,0,120.935    V7.065C0,3.162,3.162,0,7.065,0h113.871C124.837,0,128,3.162,128,7.065v113.87c0,3.902-3.163,7.065-7.064,7.065H88.318V78.431    h16.638l2.491-19.318H88.318V46.78c0-5.593,1.553-9.404,9.573-9.404l10.229-0.004V20.094c-1.769-0.235-7.841-0.761-14.906-0.761    c-14.749,0-24.846,9.003-24.846,25.535v14.246H51.688v19.318h16.681V128z" fill="#dedede" fillRule="evenodd" id="Facebook"></path></g></g></svg></a>
 
