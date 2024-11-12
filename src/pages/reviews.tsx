61b006bccfab27abe13068132b3ccf02bad4aacf
CrisSaavedra <crisisaave@gmail.com>
2024-11-12 00:06:45 -0400

    Add script and iframe to Reviews component for integration with Elfsight app for enhanced review display

--- src/pages/reviews.tsx
+++ src/pages/reviews.tsx
@@ -1,7 +1,9 @@
 function Reviews() {
-    return ( <>
-        
-    </> );
+    return (<>
+        <script src="https://static.elfsight.com/platform/platform.js" async></script>
+        {/* <iframe class="elfsight-app-68654e23-6b4a-419a-8ac6-254114274a89" data-elfsight-app-lazy></iframe> */}
+        <iframe src="68654e23-6b4a-419a-8ac6-254114274a89" style={{border:"none",  width:"100%;"}}></iframe>
+    </>);
 }
 
 export default Reviews;
