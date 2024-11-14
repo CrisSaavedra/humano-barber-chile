87c6749ce746f60b164d0379a67c9d2dc541cad5
CrisSaavedra <crisisaave@gmail.com>
2024-11-14 00:27:06 -0400

    Refactor Reviews component to dynamically load Elfsight script, enhancing widget integration and cleanup of iframe usage

--- src/pages/reviews.tsx
+++ src/pages/reviews.tsx
@@ -1,9 +1,34 @@
+import { useEffect } from "react";
+
 function Reviews() {
-    return (<>
-        <script src="https://static.elfsight.com/platform/platform.js" async></script>
-        {/* <iframe class="elfsight-app-68654e23-6b4a-419a-8ac6-254114274a89" data-elfsight-app-lazy></iframe> */}
-        <iframe src="68654e23-6b4a-419a-8ac6-254114274a89" style={{border:"none",  width:"100%;"}}></iframe>
-    </>);
+    useEffect(() => {
+        // Crear el script de Elfsight y añadirlo al documento
+        const script = document.createElement("script");
+        script.src = "https://static.elfsight.com/platform/platform.js";
+        script.async = true;
+        script.onload = () => {
+            // Configuración del widget de Elfsight
+            const elfsightApp = document.createElement("div");
+            elfsightApp.className = "elfsight-app-68654e23-6b4a-419a-8ac6-254114274a89";
+            // if (document.getElementById("elfsight-container")!.children.length < 0)
+            document.getElementById("elfsight-container")!.appendChild(elfsightApp)
+        };
+        document.body.appendChild(script);
+
+        // document.querySelector('#eapps-google-reviews-68654e23-6b4a-419a-8ac6-254114274a89 > div > a')!.classList.add('hidden');
+
+        return () => {
+            // Limpiar el script cuando se desmonte el componente
+            document.body.removeChild(script);
+        };
+    }, []);
+
+    return (
+        <div className="flex justify-center items-center">
+            <div id="elfsight-container"></div>
+        </div>
+    )
+
 }
 
 export default Reviews;
