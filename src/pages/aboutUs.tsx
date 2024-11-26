4df6b788779745074e406755bce8ce78d2b0e93e
CrisSaavedra <crisisaave@gmail.com>
2024-11-26 10:28:16 -0400

    Update AboutUs section to use 'contenedor' class for better layout structure, enhancing responsiveness and readability

--- src/pages/aboutUs.tsx
+++ src/pages/aboutUs.tsx
@@ -3,7 +3,7 @@ import pictureOne from '@/assets/picture-1.jpg';
 function AboutUs() {
     return (
         <>
-            <section className="h-[700px] relative flex justify-center items-center ">
+            <section className="contenedor relative flex justify-center items-center ">
                 <div className="h-full bg-cover bg-center absolute w-full z-[-6] grayscale" style={{ backgroundImage: `url(${pictureOne})` }}></div>
                 <main className="w-[85%] h-[90%] relative max-w-[1000px]">
                     <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black z-[-5] opacity-85"></div>
